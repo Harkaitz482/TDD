bbfbb00565b9e72fb77a2c86bbb42b74cdc45fad
Harkaitz482 <harkatrujillo@hotmail.com>
2023-03-17 17:47:30 +0000

    Cambio el test de fizzbuzz por nombre y apellidos

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -3,25 +3,6 @@ import { describe, it } from 'vitest';
 
 
 
-const fizzbuzz = (number) => {
-     if(typeof number != 'number') throw new Error('paramater provided must be a number')
-     if(Number.isNaN(number)) throw new Error('paramater provided must be a number')
- 
-     if(number % 3 == 0 && number % 5 == 0) return 'fizzbuzz'
-     if(number % 15 == 0) return 'fizzbuzz'
-     if(number % 3 == 0) return 'fizz'
-     if(number % 5 == 0) return 'buzz'
-     const multiplies = {3:'fizz',5:'buzz'}
-     let output = ''
- 
-     Object
-         .entries(multiplies)
-         .forEach(([multiplier,word]) => {
-             if(number % multiplier == 0) output += word
-         })
- 
-     return output == '' ? number : output
- }
 
 
      describe("FIZZBUZZ",() =>{
@@ -39,25 +20,32 @@ const fizzbuzz = (number) => {
               expect(fizzbuzz(1)).toBe(1)
           })
           it('should return 2 if number provided is 2',() =>{
-              expect(fizzbuzz(2)).toBe(2)
+              expect(fizzbuzz(2)).toBe('Harkaitz')
+              expect(fizzbuzz(4)).toBe('Harkaitz')
+              expect(fizzbuzz(8)).toBe('Harkaitz')
           })
           it('should return fizz if number provided is 3',() =>{
-              expect(fizzbuzz(3)).toBe('fizz')
+              expect(fizzbuzz(3)).toBe('Trujillo')
+              
+              expect(fizzbuzz(9)).toBe('Trujillo')
           })
           it('should return fizz if number provided is multiple of 3',() =>{
-              expect(fizzbuzz(6)).toBe('fizz')
-              expect(fizzbuzz(9)).toBe('fizz')
-              expect(fizzbuzz(12)).toBe('fizz')
+              
+            
+             
           })
          
-          it('should return buzz if number provided is multiple of 5',()=>{
-              expect(fizzbuzz(5)).toBe('buzz')
+          it('should return buzz if number provided is multiple of 11',()=>{
+            expect(fizzbuzz(11)).toBe('Corrales')
+            expect(fizzbuzz(55)).toBe('Corrales')
+             
           })
           it('should return buzz if number provided is multiple of 5',()=>{
-              expect(fizzbuzz(10)).toBe('buzz')
-              expect(fizzbuzz(20)).toBe('buzz')
+            
           })
-          it('should return fizzbuzz if number provided is multiple of 3 and 5',()=>{
-              expect(fizzbuzz(15)).toBe('fizzbuzz')
+          it('should return fizzbuzz if number provided is multiple of 3 and 2',()=>{
+              expect(fizzbuzz(6)).toBe('Harkaitz Trujillo')
+              expect(fizzbuzz(12)).toBe('Harkaitz Trujillo')
+              expect(fizzbuzz(18)).toBe('Harkaitz Trujillo')
           })
       })
---
+++ src/Fizzbuzz.js
@@ -0,0 +1,19 @@
+const fizzbuzz = (number) => {
+     if(typeof number != 'number') throw new Error('paramater provided must be a number')
+     if(Number.isNaN(number)) throw new Error('paramater provided must be a number')
+ 
+     if(number % 3 == 0 && number % 2 == 0) return 'Harkaitz Trujillo'
+     if(number % 11 == 0) return 'Corrales'
+     if(number % 3 == 0) return 'Trujillo'
+     if(number % 2 == 0) return 'Harkaitz'
+     const multiplies = {3:'Trujillo',2:'Harkaitz'}
+     let output = ''
+ 
+     Object
+         .entries(multiplies)
+         .forEach(([multiplier,word]) => {
+             if(number % multiplier == 0) output += word
+         })
+ 
+     return output == '' ? number : output
+ }
